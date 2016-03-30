a9a4f6312c034974f13059c12693f9e16f7b3109
artemisrj <ysyszrj@126.com>
2016-03-30 10:03:05 +0800

    格式

--- scatter/draw_scatter.js
+++ scatter/draw_scatter.js
@@ -7,7 +7,6 @@
 function draw_scatter(positions,lables,selector){
 
 	var div = d3.select(selector)[0][0];
-
 	var client_width = div.clientWidth;
 	var client_height = div.clientHeight;
 
@@ -18,27 +17,24 @@ function draw_scatter(positions,lables,selector){
 	var r = width;
 
 	var x = d3.scale.linear()
-    .range([margin.left, width]);
+		.range([margin.left, width]);
 
 	var y = d3.scale.linear()
 	    .range([margin.top,height]);
 
-	var color = ["#3366cc", "#ff9900", "#109618", "#990099", "#0099c6",
-	             "#dd4477", "#66aa00", "#b82e2e", "#316395", "#994499", "#22aa99",
-	             "#aaaa11", "#6633cc", "#e67300", "#8b0707", "#651067", "#329262",
-	             "#5574a6", "#3b3eac", "#dc3912"];
+	var color = [	"#3366cc", "#ff9900", "#109618", "#990099", "#0099c6",
+					"#dd4477", "#66aa00", "#b82e2e", "#316395", "#994499",
+					"#22aa99", "#aaaa11", "#6633cc", "#e67300", "#8b0707",
+					"#651067", "#329262", "#5574a6", "#3b3eac", "#dc3912"];
 
 	var svg = d3.select("#scatter_plot").append("svg")
 	    .attr("width", width + margin.left + margin.right)
 	    .attr("height", height + margin.top + margin.bottom)
-	  .append("g")
+		.append("g")
 	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
 
 	x.domain(d3.extent(positions, function(d) { return d[0]; })).nice();
 	y.domain(d3.extent(positions, function(d) { return d[1]; })).nice();
-	
-
 
 	svg.selectAll(".circle_class")
 		    .data(positions)
